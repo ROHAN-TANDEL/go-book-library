6a8a96e994d239b48675007e07e012d43798363b
Rohan Tandel <67rohan@gmail.com>
2026-06-04 11:00:34 +0530

    routing paramters

--- main.go
+++ main.go
@@ -5,30 +5,35 @@ import "github.com/gin-gonic/gin"
 func main() {
 	router := gin.Default()
 
-	router.GET("/get-book", getBook)
-	router.POST("/add-book", addBook)
-	router.PUT("/replace-book", replaceBook)
-	router.PATCH("/upgrade-book", upgradeBook)
-	router.DELETE("/remove-book", removeBook)
+	router.GET("/get-book/:id", getBook)
+	router.POST("/add-book/:id", addBook)
+	router.PUT("/replace-book/:id", replaceBook)
+	router.PATCH("/upgrade-book/:id", upgradeBook)
+	router.DELETE("/remove-book/:id", removeBook)
 	router.Run(":8080")
 }
 
 func getBook(c *gin.Context) {
-	c.JSON(200, gin.H{"action": "book is fetched"})
+	bookID := c.Param("id")
+	c.JSON(200, gin.H{"action": "book is fetched", "book_id": bookID})
 }
 
 func addBook(c *gin.Context) {
-	c.JSON(200, gin.H{"action": "book is added"})
+	bookID := c.Param("id")
+	c.JSON(200, gin.H{"action": "book is added", "book_id": bookID})
 }
 
 func replaceBook(c *gin.Context) {
-	c.JSON(200, gin.H{"action": "book is replaced"})
+	bookID := c.Param("id")
+	c.JSON(200, gin.H{"action": "book is replaced", "book_id": bookID})
 }
 
 func upgradeBook(c *gin.Context) {
-	c.JSON(200, gin.H{"action": "book is upgraded"})
+	bookID := c.Param("id")
+	c.JSON(200, gin.H{"action": "book is upgraded", "book_id": bookID})
 }
 
 func removeBook(c *gin.Context) {
-	c.JSON(200, gin.H{"action": "book is removed"})
+	bookID := c.Param("id")
+	c.JSON(200, gin.H{"action": "book is removed", "book_id": bookID})
 }
