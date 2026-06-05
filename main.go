7cf50f2850b3c1379ffbda5b9a22076c54a4aea7
Rohan Tandel <67rohan@gmail.com>
2026-06-05 11:33:46 +0530

    insert single record operation

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/get-book/:id", getBook)
-	router.POST("/add-book/:id", addBook)
+	router.POST("/add-book", addBook)
 	router.PUT("/replace-book/:id", replaceBook)
 	router.PATCH("/upgrade-book/:id", upgradeBook)
 	router.DELETE("/remove-book/:id", removeBook)
@@ -29,12 +29,13 @@ func main() {
 }
 
 type Book struct {
-	BookId    int `gorm:"column:book_id;primaryKey;AutoIncrement" json:"book_id"`
-	Title     string
-	language  string
-	Summary   string
-	Isbn      string
-	Publisher string
+	BookId          int `gorm:"column:book_id;primaryKey;AutoIncrement" json:"book_id"`
+	Title           string
+	Language        string
+	Summary         string
+	Isbn            string
+	Publisher       string
+	PublicationDate string `gorm:"column:publication_date" json:"publication_date"`
 }
 
 func getBook(c *gin.Context) {
@@ -48,9 +49,36 @@ func getBook(c *gin.Context) {
 	c.JSON(200, gin.H{"action": "book is fetched", "book_id": bookID, "book": books})
 }
 
+type newBook struct {
+	Title           string `json:"title"`
+	Language        string `json:"language"`
+	Summary         string `json:"summary"`
+	Isbn            string `json:"isbn"`
+	Publisher       string `json:"publisher"`
+	PublicationDate string `json:"publication_date"`
+}
+
 func addBook(c *gin.Context) {
-	bookID := c.Param("id")
-	c.JSON(200, gin.H{"action": "book is added", "book_id": bookID})
+	var newBook newBook
+	if err := c.ShouldBindJSON(&newBook); err != nil {
+		c.JSON(500, gin.H{"error": err.Error(), "message": "could not add the book"})
+	}
+
+	var record Book = Book{
+		Title:           newBook.Title,
+		Summary:         newBook.Summary,
+		Isbn:            newBook.Isbn,
+		Publisher:       newBook.Publisher,
+		PublicationDate: newBook.PublicationDate,
+		Language:        newBook.Language,
+	}
+
+	res := db.Create(&record)
+	if res.Error != nil {
+		c.JSON(500, gin.H{"error": res.Error})
+	}
+
+	c.JSON(200, gin.H{"action": "book is added", "book": newBook})
 }
 
 func replaceBook(c *gin.Context) {
